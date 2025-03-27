c9fe445cbc00fb398ae0290f317b460b141c6673
GregRos <life.gregr@gmail.com>
2025-03-27 23:28:14 +0200

    Cleanup of unneeded/old files

--- packages/the-type-is-right/lib/index.ts
+++
@@ -1,5 +0,0 @@
-// Conditional returns can enforce identical types.
-// See here: https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
-// prettier-ignore
-
-export { the_type } from "./wrapper";
